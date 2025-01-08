1557a474041ce8079dacd28facf7319587fe8b0f
ios7jbpro <71101500+ios7jbpro@users.noreply.github.com>
2025-01-08 23:27:58 +0300

    Add warning_dialog.xml in CropWallpaper.java to warn the user about this experimental feature

---
+++ app/src/main/java/com/ios7/wallify/CropWallpaper.java
@@ -0,0 +1,176 @@
+package com.ios7.wallify;
+
+import static androidx.core.content.ContextCompat.getSystemService;
+
+import android.app.WallpaperManager;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.ios7.wallify.databinding.ActivityCropWallpaperBinding;
+
+import java.io.IOException;
+
+
+public class CropWallpaper extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityCropWallpaperBinding binding;
+    private com.canhub.cropper.CropImageView cropImageView;
+    private LinearLayout cropScreen;
+    private LinearLayout confirmScreen;
+    private TextView textViewTopbar;
+    private TextView nextButton;
+    private TextView goBackButton;
+    private TextView setWallpaperButton;
+    private ImageView cropPreview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityCropWallpaperBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        cropImageView = findViewById(R.id.cropImageView);
+        cropScreen = findViewById(R.id.cropScreen);
+        confirmScreen = findViewById(R.id.confirmScreen);
+        textViewTopbar = findViewById(R.id.textViewTopbar);
+        nextButton = findViewById(R.id.nextButton);
+        goBackButton = findViewById(R.id.goBackButton);
+        setWallpaperButton = findViewById(R.id.setWallpaperButton);
+        cropPreview = findViewById(R.id.cropPreview);
+        confirmScreen.setVisibility(View.GONE);
+        goBackButton.setVisibility(View.GONE);
+        setWallpaperButton.setVisibility(View.GONE);
+        // Create the bottom sheet to warn the user
+        BottomSheetDialog bottomSheet = new BottomSheetDialog(this);
+        // Use the custom warning_dialog.xml as the bottomsheet
+        View bottomSheetView = getLayoutInflater().inflate(R.layout.warning_dialog, null);
+        // Disable default background to get rid of the square corners
+        bottomSheet.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        // Set the view to the bottomsheet
+        bottomSheet.setContentView(bottomSheetView);
+        bottomSheet.show();
+        // Setup the dismiss button by finding the view ID
+        MaterialButton dismissButton = bottomSheetView.findViewById(R.id.dissmissButton);
+        // Dismiss button click listener
+        dismissButton.setOnClickListener(new View.OnClickListener() {
+                                             @Override
+                                             public void onClick(View _view) {
+                                                 bottomSheet.dismiss();
+                                             }
+                                         });
+
+        // Next button click listener
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View _view) {
+                cropScreen.setVisibility(View.GONE);
+                confirmScreen.setVisibility(View.VISIBLE);
+                goBackButton.setVisibility(View.VISIBLE);
+                setWallpaperButton.setVisibility(View.VISIBLE);
+                nextButton.setVisibility(View.GONE);
+                textViewTopbar.setText("Preview");
+                // Show the cropped preview
+                cropPreview.setImageBitmap(cropImageView.getCroppedImage());
+            }
+        });
+
+        goBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View _view) {
+                cropScreen.setVisibility(View.VISIBLE);
+                confirmScreen.setVisibility(View.GONE);
+                goBackButton.setVisibility(View.GONE);
+                setWallpaperButton.setVisibility(View.GONE);
+                nextButton.setVisibility(View.VISIBLE);
+            }
+            });
+
+        setWallpaperButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View _view) {
+                WallpaperManager wallManager = WallpaperManager.getInstance(getApplicationContext());
+                try {
+                    Toast.makeText(getApplicationContext(), "Loading in cropped image and setting wallpaper...", Toast.LENGTH_SHORT).show();
+                    wallManager.clear(); // Clear the existing wallpaper
+                    wallManager.setBitmap(cropImageView.getCroppedImage()); // Set the loaded Bitmap as wallpaper
+                } catch (IOException ex) {
+                    ex.printStackTrace(); // Handle exceptions
+                    Toast.makeText(getApplicationContext(), "Failed to set wallpaper", Toast.LENGTH_SHORT).show();
+                }
+            }
+            });
+
+        // Get the image to load onto the cropper using the passed key from the old activity
+        Intent intent = getIntent();
+        Uri link = intent.getParcelableExtra("link");
+        // Load the link using glide into the view
+       // Glide.with(getApplicationContext())
+       //         .asBitmap()
+       //         .load(link)
+       //         .into(binding.cropImageView);
+       // cropImageView.setImageBitmap(
+       //         Glide.with(getApplicationContext())
+       //                 .asBitmap()
+       //                 .load(link)
+       // );
+
+        try {
+
+
+            Glide.with(getApplicationContext())
+                    .asBitmap()
+                    .load(link)
+                    .into(new SimpleTarget<Bitmap>() {
+                        @Override
+                        public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                            try {
+                            cropImageView.setImageBitmap(resource);
+                            } catch (Exception e) {
+                                // Show the error in a toast
+                                Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                                // Copy to clipboard
+                                String errorCode = e.getMessage();
+                                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                                ClipData clip = ClipData.newPlainText("Error:", errorCode);
+                                clipboard.setPrimaryClip(clip);
+                            }
+                        }
+                    });
+        } catch (Exception e) {
+            // Show the error in a toast
+            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
